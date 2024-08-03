a0acb30803edbf723f9df40a8bb881166f02efac
Aaryu1011 <AryanRajput7002a@gmail.com>
2024-08-03 17:39:01 +0530

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,64 @@
+
+body
+{
+    margin: 0px;
+    text-align: center;
+    font-family: "Merriweather", serif;
+}
+h1{
+     margin-top: 0px;
+     font-family: "Sacramento", cursive;
+}
+h2
+{
+    font-family: "Montserrat", sans-serif;
+}
+h3
+{
+    font-family: "Montserrat", sans-serif;
+}
+.top-container
+{
+    background-color: #e4f9f5;
+    padding-top: 350px;
+    position:relative;
+}
+.middle-container
+{
+    background-color: #e4f9f5;
+}
+.bottom-container
+{
+    background-color: #e4f9f5;
+}
+.bottom-cloud
+{
+    position: absolute; 
+    left: 300px;
+    bottom: 300px;
+    width: 20%;
+}
+.top-cloud
+{
+    position: absolute;
+    right:300px;
+    top: 200px;
+    width: 20%;
+}
+.tab
+{
+    text-align: center;
+}
+hr
+{ 
+    border-color: rgb(158, 240, 237);
+    border-style: none;
+    border-top-style: dotted;
+    border-width: 9px;
+    width: 20%;
+    
+}
+.edu
+{
+    width: 10%;
+}
